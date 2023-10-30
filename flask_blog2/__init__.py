e889d609178a7fedf4ada6959b9c4be45ec8dc6d
Yuki Shiroi <yukishiroi@protonmail.com>
2023-10-30 16:34:06 +0100

    error wekzeug

---
+++ flask_blog2/__init__.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+from sqlalchemy.sql import func
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+LoginManager = LoginManager(app)
+
+
+from flask_blog2 import routes
---
+++ run.py
@@ -0,0 +1,5 @@
+from flask_blog2 import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
